459ea5d2a3d587335aec6c21f2ec07beb84e99c8
Altynbek312 <altyn_312@mail.ru>
2023-02-26 01:15:35 +0600

    Deploy

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -60,7 +60,7 @@ export const Contact = () => {
                     isVisible ? "animate__animated animate__zoomIn" : ""
                   }
                   src={contactImg}
-                  alt="Contact Us"
+                  alt="Contact  Us"
                 />
               )}
             </TrackVisibility>
